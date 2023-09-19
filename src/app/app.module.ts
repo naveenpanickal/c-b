34565ec73273e04aa4e4d1265ab46de6ae6f8ab7
Naveen Thomas Varghese <naveen.panickal@gmail.com>
2023-09-19 14:42:45 +0100

    chore: add firebase to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { GameComponent } from './components/game/game.component';
 import { GameChildComponent } from './components/game-child/game-child.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,9 @@ import { GameChildComponent } from './components/game-child/game-child.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'bull-and-cow-a5c2b',
+    appId: '1:507966350647:web:173263d7f3a1e38195e6b2',
+    storageBucket: 'bull-and-cow-a5c2b.appspot.com',
+    apiKey: 'AIzaSyDmGKZBL6V6iAyjeofpfN1lk5elNGSsvCk',
+    authDomain: 'bull-and-cow-a5c2b.firebaseapp.com',
+    messagingSenderId: '507966350647',
+    measurementId: 'G-2QR1ZST0WR',
+  },
+};
